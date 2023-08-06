2bffd8d37b6c4e9d304e966cf4d7188662143c4d
dominhhaintq2610 <hai.do@ntq-solution.com.vn>
2023-08-06 22:36:33 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: false,
+        host: true,
+        strictPort: true,
+        port: 3000,
+        hmr: {host: 'localhost', protocol: 'ws'},
+        watch: {
+            usePolling:true,
+        }
+    },
 });
